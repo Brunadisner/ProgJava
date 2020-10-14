8cbbb32a8a82e69ca687bc5972e26b1126854693
Bruna Disner <bruna.disner@gmail.com>
2020-10-14 15:49:29 -0300

    Alterações no exer

--- ListaL02/Exer3.java
+++ ListaL02/Exer3.java
@@ -4,10 +4,14 @@ class Retangulo {
     private int base;
     private int altura;
 
-    public Retangulo() {
-        this.base = 7;
-        this.altura = 4;
+    Retangulo(int base, int altura){
+        setBase(base);
+        setAltura(altura);
 
+    }
+    public void imprimeDimensoes() {
+        System.out.println("A base eh: "+this.base);
+        System.out.println("A Altura eh: "+this.altura);
     }
     public void setBase(int base){
         this.base = base;
@@ -22,25 +26,20 @@ class Retangulo {
         return altura;
     }
     // Imprime as dimensoes do retangulo
-    public void imprimeDimensoes() {
-        System.out.println("A base eh: "+getBase());
-        System.out.println("A Altura eh: "+getAltura());
-    }
+    
 }
 public class Exer3 {
     public static void main(String[] args) {
-
-        Retangulo r = new Retangulo();
+        Retangulo r = new Retangulo(7,4);
         Scanner s = new Scanner(System.in);
         
         r.imprimeDimensoes();
 
         System.out.println("Digite o valor da Base do retangulo");
-        int base = s.nextInt();
-        r.setBase(base);
+        //int base = s.nextInt();
+        r.setBase(s.nextInt());
         System.out.println("Digite o valor da Altura do retangulo");
-        int alt = s.nextInt();
-        r.setAltura(alt);
+        r.setAltura(s.nextInt());
 
         r.imprimeDimensoes();
 
