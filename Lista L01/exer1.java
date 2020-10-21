c77234b54c43d773ff670a4c4dd72642c675accc
Bruna Gabriela Disner <bruna.disner@gmail.com>
2020-10-21 09:15:40 -0300

    Update exer1.java

--- Lista L01/exer1.java
+++ Lista L01/exer1.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
 
+class Autor{
+
+    public String nome;
+    public int id;
+
+}
+
+class Exer1{
+    public static void main(String[] args) {
+
+        Autor sr = new Autor();
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite o nome do Autor");
+        sr.nome = s.nextLine();
+        System.out.println("Digite o seu id");
+        sr.id = s.nextInt();
+
+        System.out.println("Nome: "+sr.nome);
+        
+
+        System.out.println("ID: "+sr.id);
+       
+
+
+
+
+
+
+        
+    }
+}
