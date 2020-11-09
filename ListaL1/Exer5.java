40c02dc8e45303ec1d222a468d0c5b0d870c322c
Brunadisner <bruna.disner@gmail.com>
2020-11-09 09:45:02 -0300

    add novos exer

---
+++ ListaL1/Exer5.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+class Autor {
+    private String nome;
+    private int id;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+}
+    
+class Post{
+    private Autor autor;
+    private String texto;
+
+    public Post(){
+        this.autor = new Autor();
+    }
+
+    public void setAutor(Autor autor){
+        this.autor = autor;
+    }
+    public void setTexto(String texto){
+        this.texto = texto;
+    }
+    public Autor getAutor() {
+        return autor;
+    }
+    public String getTexto() {
+        return texto;
+    }
+
+    // Imprime todas as propriedades da classe no formato:
+    public void imprimeResumo() {
+        System.out.println("Nome do autor é: "+ getAutor());
+        //System.out.println("O ID é "+ getId());
+        System.out.println("Texto: "+ getTexto());
+        
+    }
+}
+
+
+
+
+
+public class Exer5 {
+    public static void main(String[] args) {
+        Autor autor = new Autor();
+        Post post = new Post();
+        java.util.Scanner s = new Scanner(System.in);
+        System.out.println("Digite o nome do autor:");
+        autor.setNome(s.next());
+        System.out.println("Digite o id:");
+        autor.setId(s.nextInt());
+        System.out.println("Digite o texto:");
+        post.setTexto(s.next());
+        post.imprimeResumo();
+    
+
+    }
+    
+}
